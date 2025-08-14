87025d0bf4986a159a463a7d3f55906c15d76019
Blooverh <63264160+Blooverh@users.noreply.github.com>
2025-08-13 23:06:01 -0500

    update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const cpus = ref(null);
+
+onMounted( async () => {
+  try{
+    const cpusRes = await axios.get('/api/cpus/amd');
+    cpus.value = cpusRes.data;
+  }catch(err){
+    err.value = err.message
+  }
+});
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +20,12 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div v-if="cpus">
+    <ul v-for="(item, idx) in cpus" :key="item.id || idx">
+      <li>{{ item.brand }} {{  item.model }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
