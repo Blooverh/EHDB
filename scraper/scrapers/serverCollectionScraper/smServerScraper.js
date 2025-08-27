dfde049c7823ce47c3c61b92a21bbfd6abfb6586
Blooverh <63264160+Blooverh@users.noreply.github.com>
2025-08-26 23:01:13 -0500

    Update smServerScraper.js
    Finsished scraping all individual pages, just need to start functionality to extract title and check with database to save chassi and price or create new server if it does not exist in database

--- scraper/scrapers/serverCollectionScraper/smServerScraper.js
+++ scraper/scrapers/serverCollectionScraper/smServerScraper.js
@@ -57,8 +57,72 @@ export async function smCollectionServer() {
 
         console.log(collectionPages);
 
+        for(let i =0; i < collectionPages.length; i++){
+            await page.goto(collectionPages[i], {waitUntil: 'domcontentloaded', timeout: 60000});
+            console.log(`[VISITING] page ${collectionPages[i]}`);
+            serverTitles = await page.$$eval('.product-item-name', listCards => 
+                listCards.map(card => {
+                    const title = card.querySelector('a.product-item-link');
+                    return title ? title.textContent.trim() : 'No Title Found';
+                })
+            );
+
+            serverUrls = await page.$$eval('.product-item-name', listCards => 
+                listCards.map(card => {
+                    const url = card.querySelector('a.product-item-link');
+                    return url ? url.href: 'No URL Found';
+                })
+            );
+
+            serverTitles.forEach((title, index) => {
+                const url = serverUrls[index];
+                scrapedServers.push({title, url});
+            });
+        }
+        
+        console.log(scrapedServers);
+
+        let chassisArr = [];
+        let priceArr = [];
+
+        let scrapedData = [];
+
+        for(const server of scrapedServers){
+
+            if(server.title !== 'No Title Found' || server.url !== 'No URL Found'){
+                console.log(`[SCRAPING] ${server.title}`);
+                
+                await page.goto(server.url, {waitUntil: 'domcontentloaded', timeout: 60000});
+
+                chassisArr = await page.$$eval('div[data-option-name="chassis"]', el => 
+                    el.map(chassiOption => {
+                        const chassis = chassiOption.querySelector('.product-name');
+                        return chassis ? chassis.textContent.trim(): 'No Chassis Found';
+                    })
+                );
+
+                priceArr = await page.$$eval('div[data-option-name="chassis"]', el => 
+                    el.map(priceEl => {
+                        let pricing = priceEl.querySelector('span.price').textContent.trim();
+                        let priceInFloat = parseFloat(pricing.replace(/[^0-9.]/g, '')) || -1;
+                        return priceInFloat;
+                    })
+                );
+            }
+
+            chassisArr.forEach((chassis, index) => {
+                const price = priceArr[index];
+                scrapedData.push({title:server.title, link: server.url, chassis, price})
+            });
+
+            
+        }
+
+        console.log(scrapedData);
+
+
     }catch(error){
-        console.error('[ERROR] Could Not Scrape Server Monkey Servers');
+        console.error('[ERROR] Could Not Scrape Server Monkey Servers: ' + error);
         process.exit(1);
     }
 
