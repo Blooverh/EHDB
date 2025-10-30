ed51fe98b47e1527f7ca8114f088460c52a262f0
Blooverh <63264160+Blooverh@users.noreply.github.com>
2025-10-29 22:43:32 -0500

    404 Page Start

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,9 +1,45 @@
 <script setup>
+
+
+
 </script>
 
 <template>
-    404 Not Found
+    <div class="notFound-section d-flex justify-content-center align-items-center">
+        <div class="notFoundBox d-flex flex-column mb-2 align-items-center">
+            <div class="notFound-header d-flex flex-row gap-2">
+                <div class="vector-box">
+                    <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14 9.33337C19.799 9.33337 24.5 7.76637 24.5 5.83337C24.5 3.90038 19.799 2.33337 14 2.33337C8.20101 2.33337 3.5 3.90038 3.5 5.83337C3.5 7.76637 8.20101 9.33337 14 9.33337Z" stroke="#F8FAFC" stroke-width="2.33333" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3.5 5.83337V22.1667C3.5 23.095 4.60625 23.9852 6.57538 24.6416C8.54451 25.298 11.2152 25.6667 14 25.6667C16.7848 25.6667 19.4555 25.298 21.4246 24.6416C23.3938 23.9852 24.5 23.095 24.5 22.1667V5.83337" stroke="#F8FAFC" stroke-width="2.33333" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3.5 14C3.5 14.9283 4.60625 15.8185 6.57538 16.4749C8.54451 17.1313 11.2152 17.5 14 17.5C16.7848 17.5 19.4555 17.1313 21.4246 16.4749C23.3938 15.8185 24.5 14.9283 24.5 14" stroke="#F8FAFC" stroke-width="2.33333" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </div>
+                <div class="header-title">
+                    <span>EHDB</span>
+                    <span class="sub-title">Enterprise Hardware Database</span>
+                </div>
+            </div>
+            <div class="notFound-alert">
+                <span class="status-code">404</span>
+                <span class="status-msg">Page Not Found</span>
+                <span class="status-sub-msg">The hardware part you're looking for does not exist in our Database YET!</span>
+            </div>
+            <div class="notFound-btns">
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="css" scoped>
+
+    .notFound-section {
+        height: 100vh;
+    }
+
+    .notFoundBox{
+        width: 600px;
+    }
+
 </style>
